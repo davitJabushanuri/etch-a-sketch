48d250807aeb2617600448037fef2f888e8e7bef
davitJabushanuri <davitjabushanuri95@gmail.com>
2021-12-08 23:37:47 +0400

    add styles and color picker

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const divWidth = container.clientWidth;
 const numOfDivs = prompt(`enter the column width`);
 const divs = numOfDivs * numOfDivs;
 const calcWidth = divWidth / numOfDivs;
+let colorButton = document.getElementById('colorPicker');
+let color = `black`;
+
+colorButton.onchange = function () {
+	color = colorButton.value;
+};
 
 for (let i = 0; i < divs; i++) {
 	const div = document.createElement('div');
 	div.style.height = calcWidth + `px`;
 	div.style.width = calcWidth + `px`;
-	div.style.backgroundColor = `red`;
+	div.style.backgroundColor = `#fff`;
 	div.classList.add('box');
 
 	container.appendChild(div);
@@ -17,13 +23,13 @@ for (let i = 0; i < divs; i++) {
 const box = document.querySelectorAll('.box');
 box.forEach((box) => {
 	box.addEventListener('mouseover', (e) => {
-		e.target.style.backgroundColor = `black`;
+		e.target.style.backgroundColor = color;
 	});
 });
 
 const resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', () => {
 	document.querySelectorAll('.box').forEach((box) => {
-		box.style.backgroundColor = 'red';
+		box.style.backgroundColor = '#fff';
 	});
 });
